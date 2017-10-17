8373ea25735b5a59f17a312d63d9a2adf87045d0
ollynov <ollynov@gmail.com>
2017-10-17 15:40:10 -0700

    added listener on server

--- app/app.js
+++ app/app.js
@@ -2,4 +2,26 @@ var express = require("express");
 var path = require("path");
  
 var app = express();
-app.use(express.static(path.join(__dirname,"/html")));
+app.use(express.static(path.join(__dirname,"/pages")));
+
+
+
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
+
+app.post('/signin', function (req, res) {
+  var user_name=req.body.email;
+  var password=req.body.password;
+  if(user_name=='admin' && password=='admin'){
+      res.send('success');
+  }
+  else{
+    res.send('Failure');
+  }
+})
+
+app.listen(8000,function(){
+    console.log("Listening on Port: ", 8000);
+})
